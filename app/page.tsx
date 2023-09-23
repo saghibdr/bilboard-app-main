2f64bb699b99d5e671d60bf98e85cb674e6f68c2
saghi <saghi7699@gmail.com>
2023-09-23 16:12:25 +0330

    Change

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>hi</main>;
+  return <main className={styles.main}>
+    Hello mother ... !
+  </main>;
 }
